6264a93e28a3dc567ba6a88c6472acb82ec85543
jwang <jing.j.wang@intel.com>
2019-11-25 16:24:56 +0800

    add a few functions

--- calc.go
+++ calc.go
@@ -3,3 +3,6 @@ package calc
 func Add(a int, b int) int
 func Inc(a int) int
 func Dec(a int) int
+func Sum([]int64) int64
+func AAdd(a[] int, b[] int) int
+func Equal(a[]byte, b[]byte) bool
--- calc_amd64.s
+++ calc_amd64.s
@@ -2,8 +2,6 @@ TEXT ·Add(SB), $0-24
   MOVQ x+0(FP), BX
   MOVQ y+8(FP), BP
   ADDQ BP, BX
-  VPXORD Z1, Z1, Z1
-  VPADDD Z0, Z1, Z3
   MOVQ BX, ret+16(FP)
   RET
 
@@ -18,3 +16,46 @@ TEXT ·Dec(SB), $0-16
   DECQ AX
   MOVQ AX, ret+8(FP)
   RET
+
+TEXT ·Sum(SB), $0-32
+    MOVQ $0, SI
+    MOVQ sl+0(FP), BX // &sl[0], addr of the first elem
+    MOVQ sl+8(FP), CX // len(sl)
+    INCQ CX           // CX++, 因为要循环 len 次
+
+start:
+    DECQ CX
+    JZ   done
+    ADDQ (BX), SI
+    ADDQ $8, BX
+    JMP  start
+
+done:
+    MOVQ SI, ret+24(FP)
+    RET
+
+TEXT ·AAdd(SB), $0-56
+  MOVQ p+0(FP), SI
+  MOVQ q+24(FP), DI
+  VMOVUPS (SI), Z1
+  VMOVUPS (DI), Z2
+  VPADDD Z1, Z2, Z3
+  MOVQ $16, AX
+  MOVQ AX, ret+48(FP)
+  RET
+
+TEXT ·Equal(SB),7,$0
+        MOVL    len+8(FP), BX
+        MOVL    len1+32(FP), CX
+        MOVL    $0, AX
+        CMPL    BX, CX
+        JNE     eqret
+        MOVQ    p+0(FP), SI
+        MOVQ    q+24(FP), DI
+        CLD
+        REP; CMPSB
+        MOVL    $1, DX
+        CMOVLEQ DX, AX
+eqret:
+        MOVB    AX, ret+48(FP)
+        RET
