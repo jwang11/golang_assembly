3e599306ea4471f8ab94145ed633d5a9ca7309bc
jwang <jing.j.wang@intel.com>
2019-11-26 10:01:56 +0800

    fix avx512 mov instruction from single precision to int

--- calc_amd64.s
+++ calc_amd64.s
@@ -18,44 +18,45 @@ TEXT ·Dec(SB), $0-16
   RET
 
 TEXT ·Sum(SB), $0-32
-    MOVQ $0, SI
-    MOVQ sl+0(FP), BX // &sl[0], addr of the first elem
-    MOVQ sl+8(FP), CX // len(sl)
-    INCQ CX           // CX++, 因为要循环 len 次
+  MOVQ $0, SI
+  MOVQ sl+0(FP), BX // &sl[0], addr of the first elem
+  MOVQ sl+8(FP), CX // len(sl)
+  INCQ CX
 
 start:
-    DECQ CX
-    JZ   done
-    ADDQ (BX), SI
-    ADDQ $8, BX
-    JMP  start
+  DECQ CX
+  JZ   done
+  ADDQ (BX), SI
+  ADDQ $8, BX
+  JMP  start
 
 done:
-    MOVQ SI, ret+24(FP)
-    RET
+  MOVQ SI, ret+24(FP)
+  RET
 
-TEXT ·AAdd(SB), $0-56
+TEXT ·AAdd(SB), $64-56
   MOVQ p+0(FP), SI
   MOVQ q+24(FP), DI
-  VMOVUPS (SI), Z1
-  VMOVUPS (DI), Z2
+  VMOVDQU32 (SI), Z1
+  VMOVDQU32 (DI), Z2
   VPADDD Z1, Z2, Z3
-  MOVQ $16, AX
+  VMOVDQU32 Z3, d0-64(SP)
+  MOVQ d0-48(SP), AX
   MOVQ AX, ret+48(FP)
   RET
 
 TEXT ·Equal(SB),7,$0
-        MOVL    len+8(FP), BX
-        MOVL    len1+32(FP), CX
-        MOVL    $0, AX
-        CMPL    BX, CX
-        JNE     eqret
-        MOVQ    p+0(FP), SI
-        MOVQ    q+24(FP), DI
-        CLD
-        REP; CMPSB
-        MOVL    $1, DX
-        CMOVLEQ DX, AX
+  MOVL    len+8(FP), BX
+  MOVL    len1+32(FP), CX
+  MOVL    $0, AX
+  CMPL    BX, CX
+  JNE     eqret
+  MOVQ    p+0(FP), SI
+  MOVQ    q+24(FP), DI
+  CLD
+  REP; CMPSB
+  MOVL    $1, DX
+  CMOVLEQ DX, AX
 eqret:
-        MOVB    AX, ret+48(FP)
-        RET
+  MOVB    AX, ret+48(FP)
+  RET
